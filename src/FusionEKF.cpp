b9ec7d36ce5bba45c80fb2eeb82b0461fea41057
Guillermo Gómez <mlgxmez@gmail.com>
2017-12-03 09:55:01 +0100

    Fixed a bug in previous_timestamp_ that had value zero.

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -105,6 +105,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       ekf_.x_(3) = 0.0;
     }
 
+    previous_timestamp_ = measurement_pack.timestamp_;
 
 
     // done initializing, no need to predict or update
@@ -155,6 +156,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the state and covariance matrices.
    */
 
+ 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
     
@@ -169,10 +171,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.R_ = R_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
     }
-
   // print the output
   cout << "x_ = " << ekf_.x_ << endl;
-  cout << "H_ = " << ekf_.H_ << endl;
   cout << "P_ = " << ekf_.P_ << endl;
 
 }
